fd8d9300f52e286c59a21042ddded0d971482f2f
nurealammiaji <nurealam.miaji@gmail.com>
2023-12-06 00:23:00 +0600

    mongodb driver added

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 5000;
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const secret = process.env.ACCESS_TOKEN_SECRET;
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
-
-const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("Tasty Food Server");
@@ -17,3 +20,31 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Tasty Food Server is running on port: ${port}`);
 })
+
+
+// MongoDB Driver
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${user}:${pass}@cluster0.31s3qjy.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
