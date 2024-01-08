704d1b14527ccd2f1243e69a152d61a200c6a217
nurealammiaji <nurealam.miaji@gmail.com>
2024-01-08 19:23:18 +0600

    data loaded form mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const express = require('express');
 const app = express();
@@ -13,17 +14,17 @@ const secret = process.env.ACCESS_TOKEN_SECRET;
 app.use(cors());
 app.use(express.json());
 
+// Server URL
 app.get("/", (req, res) => {
-    res.send("Tasty Food Server");
+  res.send("Tasty Food Server");
 })
 
 app.listen(port, () => {
-    console.log(`Tasty Food Server is running on port: ${port}`);
+  console.log(`Tasty Food Server is running on port: ${port}`);
 })
 
 
 // MongoDB Driver
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${user}:${pass}@cluster0.31s3qjy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,7 +39,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    client.connect();
+    await client.connect();
+
+    const menuCollection = client.db("tastyFood").collection("menus");
+    const reviewCollection = client.db("tastyFood").collection("reviews");
+
+    app.get("/menus", async (req, res) => {
+      const cursor = menuCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
