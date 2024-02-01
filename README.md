8cc3a7a8ff5a5e2379e32225240f6b44da5bd1de
nurealammiaji <nurealam.miaji@gmail.com>
2024-02-01 20:49:43 +0600

    readme file added

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tasty Food
+
+A Restaurant Web Application built with React, Node, Express, MongoDB, Firebase and JWT Authentication.
+
+Live sites are available:
+
+- Live Website: https://tastyfoodbd.web.app/
+
+- Live Server: https://tasty-food-server-bd.vercel.app/
--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     const menuCollection = client.db("tastyFood").collection("menus");
     const reviewCollection = client.db("tastyFood").collection("reviews");
